a1da73e97d6fc2b9e3c62da601ac16bd80c3b4e6
pirate <132884539+purrate@users.noreply.github.com>
2023-11-26 23:27:11 +0530

    push before the redesign

--- src/app/loading/page.tsx
+++ src/app/loading/page.tsx
@@ -4,6 +4,7 @@ import off from '@/assets/images/off-button.svg';
 import { useState, useEffect } from 'react';
 import on from '@/assets/images/on-button.svg';
 import press from '@/assets/images/press-button.svg'
+import green from '@/assets/images/press-green.svg'
 import Image from 'next/image';
 
 import './styles.css'
@@ -12,72 +13,94 @@ const loadingPage = () => {
 
   const [button1, setButton1] = useState(false);
   const [button2, setButton2] = useState(true);
-  const [currentTime, setCurrentTime] = useState<Date>(new Date());
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const [buttonClicked1, setButtonClicked1] = useState(false);
+  const [buttonClicked2, setButtonClicked2] = useState(false);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+  const [intervalDuration, setIntervalDuration] = useState(1000);
+  const [year, setYear] = useState(String(new Date().getFullYear()).padStart(4, '0'));
+  const [month, setMonth] = useState(new Date().toLocaleString('default', { month: 'short' }));
+  const [date, setDate] = useState(String(new Date().getDate()).padStart(2, '0'));
+  const [hours, setHours] = useState(String(new Date().getHours()).padStart(2, '0'));
+  const [minutes, setMinutes] = useState(String(new Date().getMinutes()).padStart(2, '0'));
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setButton1(prevButton1 => !prevButton1);
-      setButton2(prevButton2 => !prevButton2);
-    }, 800);
+    if (!buttonClicked2) {
+      const timer = setInterval(() => {
+        setYear(String(new Date().getFullYear()).padStart(4, '0'));
+        setMonth(new Date().toLocaleString('default', { month: 'short' }));
+        setDate(String(new Date().getDate()).padStart(2, '0'));
+        setHours(String(new Date().getHours()).padStart(2, '0'));
+        setMinutes(String(new Date().getMinutes()).padStart(2, '0'));
+      }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
+      return () => clearInterval(timer);
+    }
+  }, [buttonClicked2]);
 
-  useEffect(() => {
-    var button = document.getElementById('press');
-
-    if (button?.className === 'press') {
-      const inter = setInterval(() => {
-        setCurrentTime(new Date());
-      }, 800);
-
-      return () => clearInterval(inter);
-    } 
-
-  }, [])
-
-  const formatTime = (time: number): string => {
-    return time < 10 ? '0' + time : time.toString();
-  };
-
-  const getRandomDate = () => {
-    const year = Math.floor(Math.random() * 100) + 1900;
-    const month = Math.floor(Math.random() * 12);
-    const day = Math.floor(Math.random() * 28) + 1;
-    const hours = Math.floor(Math.random() * 24);
-    const minutes = Math.floor(Math.random() * 60);
-    return new Date(year, month, day, hours, minutes);
-  };
-
-  const endPoint = () => {
-    var month = document.getElementById('m-text')
-    var date = document.getElementById('d-text')
-    var year = document.getElementById('y-text')
-    var hours = document.getElementById('h-text')
-    var minutes = document.getElementById('mi-text')
-    var ready = document.getElementById('ready-text')
-
-    month!.innerHTML = '---'
-    date!.innerHTML = '--'
-    year!.innerHTML = '----'
-    hours!.innerHTML = '--'
-    minutes!.innerHTML = '--'
-
-    ready!.innerHTML = '????? ?? ?????? ?'
+  const generateRandomCharacters = (length: number) => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
   }
 
+  const generateRandomNumber = (length: number) => {
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += Math.floor(Math.random() * 10).toString();
+    }
+    return result;
+  }
+
+  useEffect(() => {
+    if (buttonClicked && !intervalId) {
+      const id = setInterval(() => {
+        setButton1(prevButton1 => !prevButton1);
+        setButton2(prevButton2 => !prevButton2);
+      }, intervalDuration);
+      setIntervalId(id);
+
+      const timer = setInterval(() => {
+        setYear(generateRandomNumber(4));
+        setMonth(generateRandomCharacters(3));
+        setDate(generateRandomNumber(2));
+        setHours(generateRandomNumber(2));
+        setMinutes(generateRandomNumber(2));
+      }, 50);
+
+      setIntervalId(timer);
+
+      setTimeout(() => {
+        setButtonClicked(false);
+        clearInterval(id);
+        clearInterval(timer);
+        setIntervalId(null);
+        setYear("----");
+        setMonth("---");
+        setDate("--");
+        setHours("--");
+        setMinutes("--");
+        var ready = document.getElementById('ready-text')
+        ready!.innerHTML = '?????? ?????? ??????'
+      }, 1690);
+    } else if (!buttonClicked && intervalId) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+  }, [buttonClicked, intervalId]);
+
+
   const handlePress = () => {
+
+    setButtonClicked(true);
+    setButtonClicked1(true);
+    setButtonClicked2(true);
     var button = document.getElementById('press');
-    if (button?.className === 'press') button.className = 'press-after'
-
-    const intp = setInterval(() => {
-      setCurrentTime(getRandomDate());
-    }, 500);
-
-    setTimeout(() => {
-      clearInterval(intp);
-      endPoint()
-    }, 10000);
+    if (button?.className === 'press') button.className = 'after'
+    setIntervalDuration(prevDuration => prevDuration / 4);
 
   }
 
@@ -95,10 +118,10 @@ const loadingPage = () => {
             </div>
             <div className="blink-buttons">
               <div className="on-button">
-                <Image src={button1 ? on : off} alt="Button 1" />
+                <Image src={button1 ? off : on} alt="Button 1" suppressHydrationWarning />
               </div>
               <div className="off-button">
-                <Image src={button2 ? on : off} alt="Button 2" />
+                <Image src={button2 ? off : on} alt="Button 2" suppressHydrationWarning />
               </div>
             </div>
           </div>
@@ -109,28 +132,28 @@ const loadingPage = () => {
           <div className="middle-row">
             <div className="mdy-text">
               <div className="month-div">
-                <div id = 'm-text' className="m-text">{currentTime.toLocaleString('default', { month: 'short' }).toUpperCase()}</div>
+                <div id='m-text' className="m-text">{month}</div>
                 <div className="m-bg-text">888</div>
               </div>
               <div className="date-div">
-                <div id = 'd-text' className="d-text">{formatTime(currentTime.getDate())}</div>
+                <div id='d-text' className="d-text">{date}</div>
                 <div className="d-bg-text">88</div>
               </div>
               <div className="year-div">
-                <div id = 'y-text' className="y-text">{currentTime.getFullYear()}</div>
+                <div id='y-text' className="y-text">{year}</div>
                 <div className="y-bg-text">8888</div>
               </div>
             </div>
             <div className="time-text">
               <div className="hours-div">
-                <div id = 'h-text' className="h-text">{formatTime(currentTime.getHours())}</div>
+                <div id='h-text' className="h-text">{hours}</div>
                 <div className="h-bg-text">88</div>
               </div>
               <div className="colon-div">
                 <div className="colon-text">:</div>
               </div>
               <div className="min-div">
-                <div id = 'mi-text' className="mi-text">{formatTime(currentTime.getMinutes())}</div>
+                <div id='mi-text' className="mi-text">{minutes}</div>
                 <div className="mi-bg-text">88</div>
               </div>
             </div>
@@ -142,13 +165,13 @@ const loadingPage = () => {
 
           <div className="bottom-row">
             <div className="ready-to">
-              <div id = 'ready-text' className="ready-text">
+              <div id='ready-text' className="ready-text">
                 READY TO TRAVEL ?
               </div>
             </div>
             <div className="press-button">
-              <button id='press' className='press' onClick={handlePress}>
-                <Image src={press} alt='' />
+              <button id='press' className={buttonClicked1 ? 'after' : 'press'} onClick={handlePress} >
+                <Image src={buttonClicked1 ? press : green} alt='' />
               </button>
             </div>
           </div>
