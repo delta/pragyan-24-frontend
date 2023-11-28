518be7c325c038f76a7901fb75765ca95f9f522e
shubham-1806 <shub.agarwal2003@gmail.com>
2023-11-29 00:33:34 +0530

    fix: overflow on ring animation

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,12 +6,11 @@ import rings from '@/assets/images/rings.svg';
 import { NavBar, SideBar } from '@/components';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { WheelEvent, TouchEvent, useState, useRef } from 'react';
+import { WheelEvent, TouchEvent, useState } from 'react';
 
 export default function Home() {
     const [isScrolled, setIsScrolled] = useState<boolean>(false);
     const router = useRouter();
-    const ringsParDiv = useRef<HTMLDivElement>(null);
     const simulateScroll = (event: WheelEvent<HTMLDivElement>) => {
         if (!isScrolled) {
             event.stopPropagation();
@@ -53,7 +52,8 @@ export default function Home() {
         <>
             <div
                 className={
-                    'absolute h-[100vh] w-[100vw] z-50 ' + (isScrolled ? ' blue-scroll' : ' hidden')
+                    'absolute h-[100vh] w-[100vw] z-50 overflow-y-hidden ' +
+                    (isScrolled ? ' blue-scroll' : ' hidden')
                 }
             ></div>
             <div
@@ -66,17 +66,16 @@ export default function Home() {
                 <NavBar />
                 <div
                     className={
-                        'max-lg:w-[60%] max-xl:w-[45%] max-2xl:w-[35%] w-[22%] h-[50%] absolute left-1/2 -translate-x-1/2 flex justify-center top-[15%]' +
+                        'max-lg:w-[60%] max-xl:w-[45%] max-2xl:w-[35%] w-[22%] h-[50%] absolute left-1/2 -translate-x-1/2 flex justify-center top-[15%] max-h-[90vh]' +
                         (isScrolled ? '' : ' hidden')
                     }
-                    ref={ringsParDiv}
                 >
                     <Image
                         src={anim}
                         alt="Animation"
                         draggable={false}
                         className={
-                            'w-[100%] md:bottom-[45%] bottom-[45%] absolute inset-0 m-auto' +
+                            'w-[100%] md:bottom-[45%] bottom-[45%] absolute inset-0 m-auto max-h-[90vh] xl:max-w-[20vw] ' +
                             (isScrolled ? ' fullrings' : '')
                         }
                     />
@@ -85,7 +84,7 @@ export default function Home() {
                         alt="Rings"
                         draggable={false}
                         className={
-                            'w-[100%] md:bottom-[18%] bottom-[18%] absolute inset-0 m-auto' +
+                            'w-[100%] md:bottom-[18%] bottom-[18%] absolute inset-0 m-auto max-h-[90vh] xl:max-w-[20vw] ' +
                             (isScrolled ? ' rings' : '')
                         }
                     />
@@ -94,13 +93,13 @@ export default function Home() {
                         alt="Rings"
                         draggable={false}
                         className={
-                            'w-[100%] md:bottom-[100%] bottom-[80%]  absolute inset-0 m-auto' +
+                            'w-[100%] md:bottom-[100%] bottom-[80%]  absolute inset-0 m-auto max-h-[90vh] xl:max-w-[20vw] ' +
                             (isScrolled ? ' rings2' : '')
                         }
                     />
                 </div>
                 <p className="font-ROG 2xl:text-9xl xl:text-8xl lg:text-7xl sm:text-5xl text-3xl mt-24">
-                    AD INFINITUM
+                    CHRONOCLE
                 </p>
                 <Image
                     src={wingman}
