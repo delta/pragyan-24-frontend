d58f5184199c4fc18f5e51c47723d44da751fdf1
pirate <132884539+purrate@users.noreply.github.com>
2023-11-29 23:07:49 +0530

    Fixed footer lint issue

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,12 +5,14 @@ import { headers } from 'next/headers';
 const Footer = () => {
     const footerText = `Made with <3 by Delta and Graphique`;
     const headersList = headers();
-    const domain = headersList.get('host') || "";
-    const fullUrl = headersList.get('referer') || "";
-    const [, pathname] = fullUrl.match(new RegExp(`https?:\/\/${domain}(.*)`)) || [];
+    const domain = headersList.get('host') || '';
+    const fullUrl = headersList.get('referer') || '';
+    const [, pathname] = fullUrl.match(new RegExp(`https?://${domain}(.*)`)) || [];
     return (
         <>
-            {pathname == '/' ? <></> :
+            {pathname == '/' ? (
+                <></>
+            ) : (
                 <div className="absolute w-[90vw] h-14 bottom-4 left-1/2 -translate-x-1/2 flex justify-center lg:justify-between items-center px-10 footer-bg font-Orbitron">
                     <div className="lg:basis-1/5 max-lg:hidden">
                         <Clock />
@@ -18,11 +20,10 @@ const Footer = () => {
                     <div className="lg:basis-3/5 flex justify-center items-center">
                         <p className="lg:text-lg sm:text-sm text-xs text-center">{footerText}</p>
                     </div>
-                    <div className="lg:basis-1/5 max-lg:hidden">
-                    </div>
+                    <div className="lg:basis-1/5 max-lg:hidden"></div>
                     <Social />
                 </div>
-            }
+            )}
         </>
     );
 };
