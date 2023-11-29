7bdaa584c70ea5ded0d7d7c5be99de538935d939
pirate <132884539+purrate@users.noreply.github.com>
2023-11-29 23:32:01 +0530

    fixed build issues

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,29 +1,29 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
 import Clock from '../Clock/Clock';
 import Social from './Social';
-import { headers } from 'next/headers';
 
 const Footer = () => {
+    const pathname = usePathname();
     const footerText = `Made with <3 by Delta and Graphique`;
-    const headersList = headers();
-    const domain = headersList.get('host') || '';
-    const fullUrl = headersList.get('referer') || '';
-    const [, pathname] = fullUrl.match(new RegExp(`https?://${domain}(.*)`)) || [];
+
+    if (pathname === '/') {
+        return null;
+    }
+
     return (
         <>
-            {pathname == '/' ? (
-                <></>
-            ) : (
-                <div className="absolute w-[90vw] h-14 bottom-4 left-1/2 -translate-x-1/2 flex justify-center lg:justify-between items-center px-10 footer-bg font-Orbitron">
-                    <div className="lg:basis-1/5 max-lg:hidden">
-                        <Clock />
-                    </div>
-                    <div className="lg:basis-3/5 flex justify-center items-center">
-                        <p className="lg:text-lg sm:text-sm text-xs text-center">{footerText}</p>
-                    </div>
-                    <div className="lg:basis-1/5 max-lg:hidden"></div>
-                    <Social />
+            <div className="absolute w-[90vw] h-14 bottom-4 left-1/2 -translate-x-1/2 flex justify-center lg:justify-between items-center px-10 footer-bg font-Orbitron">
+                <div className="lg:basis-1/5 max-lg:hidden">
+                    <Clock />
                 </div>
-            )}
+                <div className="lg:basis-3/5 flex justify-center items-center">
+                    <p className="lg:text-lg sm:text-sm text-xs text-center">{footerText}</p>
+                </div>
+                <div className="lg:basis-1/5 max-lg:hidden"></div>
+                <Social />
+            </div>
         </>
     );
 };
