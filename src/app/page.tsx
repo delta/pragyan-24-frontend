18544eeb2683d07482c040df60569ae8b709ecab
shubham-1806 <shub.agarwal2003@gmail.com>
2023-12-01 20:58:28 +0530

    feat: loader(web)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,45 @@
-import wingman from '@/assets/images/wingman.png';
-import { NavBar } from '@/components';
 import Image from 'next/image';
+import offLight from '@/assets/images/OffLight.svg';
 
-export default function Home() {
+export default function Loading() {
     return (
         <>
-            <div className="min-h-screen w-full text-center lg:p-7 p-5 home ">
-                <NavBar />
-                <p className="font-ROG 2xl:text-9xl xl:text-8xl lg:text-7xl sm:text-5xl text-3xl mt-24">
-                    CHRONOCLE
-                </p>
-                <Image
-                    src={wingman}
-                    alt="Wingman"
-                    draggable={false}
-                    className="wingman lg:w-48 md:w-32 w-20 absolute inset-0 m-auto bottom-1/3"
-                />
-                <p className="font-ROG xl:text-5xl lg:text-4xl md:text-3xl sm:text-2xl text-xl">
-                    A TIMELESS ODYSSEY
-                </p>
+            <div className="w-[100vw] h-[100vh] bg-black relative">
+                <div className="relative left-[50%] top-[50%] transform -translate-x-1/2 -translate-y-1/2 h-[90%] w-[100%] loadingBg flex flex-col items-center justify-center">
+                    <div className=" w-[80%] h-[28%] loadingdivs relative flex items-center justify-end">
+                        <div className=" mr-[2%] flex w-[10%] h-[100%] items-end 2xl:h-[100%] lights">
+                            <Image src={offLight} alt="light" />
+                            <Image src={offLight} alt="light" />
+                        </div>
+                    </div>
+                    <div className=" w-[80%] h-[45%] loadingdivs flex flex-col relative">
+                        <div className="h-[20%] text-center w-[100%] xl:mt-0 rogfont">
+                            PRESENT TIME
+                        </div>
+                        <div className="h-[60%] w-[100%] flex relative justify-evenly mt-[5%]">
+                            <div className="h-[60%] w-[15%] bg-[#1D1D1D] flex items-center justify-center sevensegtext">
+                                666
+                            </div>
+                            <div className="h-[60%] w-[10%] bg-[#1D1D1D] flex items-center justify-center sevensegtext">
+                                66
+                            </div>
+                            <div className="h-[60%] w-[20%] bg-[#1D1D1D] flex items-center justify-center sevensegtext">
+                                6666
+                            </div>
+                            <div className="h-[60%] w-[10%] bg-[#1D1D1D] flex items-center justify-center sevensegtext">
+                                66
+                            </div>
+                            <div className="h-[60%] w-[10%] bg-[#1D1D1D] flex items-center justify-center sevensegtext">
+                                66
+                            </div>
+                        </div>
+                        <div className="h-[20%] text-center w-[100%] rogfont">READY TO TRAVEL?</div>
+                    </div>
+                    <div className=" w-[80%] h-[27%] loadingdivs flex justify-end relative">
+                        {/* <Image src={greenPress} alt='button' className=' h-[60%] w-[20%]'/> */}
+                    </div>
+                </div>
             </div>
-            {/* <About /> */}
         </>
     );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,13 @@
+'use client';
 import Clock from '../Clock/Clock';
 import Social from './Social';
+import { usePathname } from 'next/navigation';
 
 const Footer = () => {
+    const pathname = usePathname();
+    if (pathname === '/') {
+        return null;
+    }
     const footerText = `Made with <3 by Delta and Graphique`;
     return (
         <div className="absolute w-[90vw] h-14 bottom-4 left-1/2 -translate-x-1/2 flex justify-center lg:justify-between items-center px-10 footer-bg font-Orbitron">
