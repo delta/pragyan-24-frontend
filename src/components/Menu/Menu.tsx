e9ab6106bba2c45985163545bc806b3bf8619e9e
Bhoopesh <82646284+bhoopesh369@users.noreply.github.com>
2023-12-09 02:16:39 +0530

    feat: menubar (#11)
    Co-authored-by: SRIMANKS <kssrisowrirajan@gmail.com>

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,93 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import MenuItem from '@/components/MenuItem/MenuItem';
+import disk from '../../assets/images/disk.png';
+import diskglow from '../../assets/images/diskglow.png';
+import styles from '../NavBar/navbar.module.css';
+
+interface MenuProps {
+    isOpened: boolean;
+    setIsOpened?: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Menu: React.FC<MenuProps> = ({ isOpened, setIsOpened }) => {
+    const [width, setWidth] = useState<number>(0);
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            setWidth(window.innerWidth);
+            window.addEventListener('resize', () => setWidth(window.innerWidth));
+        }
+    }, []);
+
+    const menuItems = [
+        { name: 'Events', link: '/events' },
+        { name: 'Workshops', link: '/workshops' },
+        { name: 'Guest Lectures', link: '/guest-lectures' },
+        { name: 'Sponsors', link: '/sponsors' },
+        { name: 'Sangam Hardware', link: '/sangam-hardware' },
+        { name: 'Campus Ambassador', link: '/campus-ambassador' },
+        { name: 'Ingenium', link: '/ingenium' },
+        { name: 'Patronages', link: '/patronages' },
+        { name: 'Youth Summit', link: '/youth-summit' },
+        { name: 'Hospitality', link: '/hospitality' },
+        { name: 'Campus', link: '/campus' },
+    ];
+
+    return (
+        <div>
+            {isOpened ? (
+                <div className="flex w-full justify-center items-center absolute z-10 top-0 left-0  bg-black">
+                    <div
+                        className="absolute top-10 right-10 z-[20] text-white text-3xl hover:cursor-pointer"
+                        onClick={() => {
+                            if (setIsOpened) setIsOpened(false);
+                        }}
+                    >
+                        X
+                    </div>
+                    <Image
+                        src={disk}
+                        alt="disk"
+                        className="absolute bottom-4 lg:bottom-0"
+                        id={styles.disk}
+                    />
+                    <Image
+                        src={diskglow}
+                        alt="diskglow"
+                        className="absolute bottom-4"
+                        id={styles.glow}
+                    />
+
+                    <div
+                        className=" lg:top-1/5  absolute w-full lg:h-2/3 h-full"
+                        id={styles.menubg}
+                    />
+                    <ul
+                        className="  md:flex w-full font-Orbitron flex lg:flex-row flex-col justify-evenly items-center  pb-24  md:[40vh%]  min-h-screen  text-white"
+                        id={styles.mainContent}
+                    >
+                        {menuItems.map((item, index) => (
+                            <li className={styles.li} key={index}>
+                                <Link href={item.link}>
+                                    <MenuItem name={item.name} phone={width < 1020} />
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <style>
+                        {`
+
+`}
+                    </style>
+                </div>
+            ) : (
+                ''
+            )}
+        </div>
+    );
+};
+
+export default Menu;
--- src/components/index.ts
+++ src/components/index.ts
@@ -11,3 +11,4 @@ export { default as Carousel } from './Carousel/carousel';
 export { default as LoadingWebView } from './Landing/LoadingWebView';
 export { default as LoadingMobileView } from './Landing/LoadingMobileView';
 export { default as ClusterCarousel } from './ClusterCarousel/ClusterCarousel';
+export { default as Menu } from './Menu/Menu';
