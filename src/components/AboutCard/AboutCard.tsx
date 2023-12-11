00cd359feb1271e72b2e370d336193e88636697e
Bhoopesh <bhoopesh459@gmail.com>
2023-12-11 23:27:43 +0530

    wip: swiper and aboutsPage

--- src/components/AboutCard/AboutCard.tsx
+++ src/components/AboutCard/AboutCard.tsx
@@ -7,7 +7,7 @@ const AboutCard: React.FC<AboutCardProps> = ({ cardRef }) => {
         <div className="AboutCard max-md:hidden h-fit sm:w-[85%] sm:h-[70%] md:w-[70%] md:h-[65%] lg:w-[60%] lg:h-[70%] xl:w-[45%] xl:h-[55%] flex justify-center items-center p-10 ">
             <p
                 ref={cardRef}
-                className="pt-3 font-Nunito max-lg:text-base text-lg md:h-[80%] lg:h-[85%] overflow-auto"
+                className="pt-3 font-Nunito max-lg:text-base text-lg md:h-[80%] lg:h-[85%]"
             >
                 {content}
             </p>
--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,90 +1,90 @@
-import React, { createContext, useCallback, useState, useEffect, ReactNode } from "react";
-import { API_URL } from "../config/config";
+import React, { createContext, useCallback, useState, useEffect, ReactNode } from 'react';
+import { API_URL } from '../config/config';
 
 export const userContext = createContext<UserContext>({
-	isLoggedIn: false,
-	loadingAuth: true,
-	userID: undefined,
-	userName: undefined,
-	qrCode: undefined,
-	setUserName: () => undefined,
-	setIsLoggedIn: () => false,
-	setuserID: () => undefined,
-	userData: null,
+    isLoggedIn: false,
+    loadingAuth: true,
+    userID: undefined,
+    userName: undefined,
+    qrCode: undefined,
+    setUserName: () => undefined,
+    setIsLoggedIn: () => false,
+    setuserID: () => undefined,
+    userData: null,
 });
 
-const UserContextProvider = ({ children }: {children : ReactNode}) => {
-	const [userID, setuserID] = useState<number | undefined>();
-	const [userName, setUserName] = useState<string | undefined>();
-	const [qrCode, setQr] = useState<string | undefined>("");
-	const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-	const [loadingAuth, setLoadingAuth] = useState<boolean>(true);
-	const [userData, setUserData] = useState<UserContext["userData"]>(null);
+const UserContextProvider = ({ children }: { children: ReactNode }) => {
+    const [userID, setuserID] = useState<number | undefined>();
+    const [userName, setUserName] = useState<string | undefined>();
+    const [qrCode, setQr] = useState<string | undefined>('');
+    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+    const [loadingAuth, setLoadingAuth] = useState<boolean>(true);
+    const [userData, setUserData] = useState<UserContext['userData']>(null);
 
-	const isAuth = useCallback(() => {
-		setLoadingAuth(true);
-		fetch(`${API_URL}/user/details`, {
-			method: "GET",
-			credentials: "include",
-			headers: {
-				"Content-Type": "application/json",
-			},
-		})
-			.then((res) => res.json())
-			.then((response) => {
-				if (response.status_code === 401) {
-					setIsLoggedIn(false);
-				} else {
-					setIsLoggedIn(true);
-				}
-				setLoadingAuth(false);
-				setUserData(response.message);
-			})
-			.catch((e) => {
-				console.log(e);
-				setLoadingAuth(false);
-			});
-	}, [userID]);
+    const isAuth = useCallback(() => {
+        setLoadingAuth(true);
+        fetch(`${API_URL}/user/details`, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(res => res.json())
+            .then(response => {
+                if (response.status_code === 401) {
+                    setIsLoggedIn(false);
+                } else {
+                    setIsLoggedIn(true);
+                }
+                setLoadingAuth(false);
+                setUserData(response.message);
+            })
+            .catch(e => {
+                console.log(e);
+                setLoadingAuth(false);
+            });
+    }, [userID]);
 
-	const getQr = useCallback(() => {
-		fetch(`${API_URL}/pr/qr`, {
-			method: "POST",
-			credentials: "include",
-			headers: {
-				"Content-Type": "application/json",
-			},
-		})
-			.then((res) => res.json())
-			.then((data) => {
-				if (data.status_code === 200) {
-					setQr(data.message);
-				} else setQr("");
-			})
-			.catch((e) => console.log(e));
-	}, [loadingAuth, isLoggedIn]);
+    const getQr = useCallback(() => {
+        fetch(`${API_URL}/pr/qr`, {
+            method: 'POST',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.status_code === 200) {
+                    setQr(data.message);
+                } else setQr('');
+            })
+            .catch(e => console.log(e));
+    }, [loadingAuth, isLoggedIn]);
 
-	useEffect(() => {
-		isAuth();
-		getQr();
-	}, [isAuth]);
+    useEffect(() => {
+        isAuth();
+        getQr();
+    }, [isAuth]);
 
-	return (
-		<userContext.Provider
-			value={{
-				setIsLoggedIn,
-				setuserID,
-				setUserName,
-				isLoggedIn,
-				loadingAuth,
-				userID,
-				userName,
-				qrCode,
-				userData,
-			}}
-		>
-			{children}
-		</userContext.Provider>
-	);
+    return (
+        <userContext.Provider
+            value={{
+                setIsLoggedIn,
+                setuserID,
+                setUserName,
+                isLoggedIn,
+                loadingAuth,
+                userID,
+                userName,
+                qrCode,
+                userData,
+            }}
+        >
+            {children}
+        </userContext.Provider>
+    );
 };
 
 export default UserContextProvider;
