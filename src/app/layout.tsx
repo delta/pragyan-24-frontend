9da91077d48a79b31e927e501eb841061bb5f5a9
Vignesh Duraisamy <vigneshd332@gmail.com>
2023-12-12 15:17:32 +0530

    fix: my skill issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
 import Footer from '@/components/Footer/Footer';
 import './globals.css';
 import type { Metadata } from 'next';
-import { content } from '@/components/AboutCard/AboutContent.json';
+import content from '@/components/AboutCard/AboutContent.json';
 import Toast from '@/components/Toast/Toast';
 
 export const metadata: Metadata = {
     title: "Pragyan '24 | Let's Celebrate Technology",
-    description: content,
+    description: content.content,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
+            <body>
                 <div className="w-full min-h-screen bg-[#070B12]">{children}</div>
                 <Footer />
                 <Toast />
