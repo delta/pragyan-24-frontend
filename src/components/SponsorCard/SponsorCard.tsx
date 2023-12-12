01bc9d7488e7efccbdf4f135f4ee6e8049a6f337
Bhoopesh <bhoopesh459@gmail.com>
2023-12-12 22:31:37 +0530

    wip: sponsors

---
+++ src/components/SponsorCard/SponsorCard.tsx
@@ -0,0 +1,29 @@
+import styles from './styles.module.css';
+
+import topLeftBorder from '../../assets/images/top-left-border.svg';
+import topRightBorder from '../../assets/images/top-right-border.svg';
+import bottomBorder from '../../assets/images/bottom-border.svg';
+import Image from 'next/image';
+
+const SponsorCard = ({ name, logo }: SponsorCardProps) => {
+    return (
+        <div className={styles.parent}>
+            <div className={styles.borderOverlay}>
+                <div className="flex w-full justify-between">
+                    <Image src={topLeftBorder} alt="" className={styles.topLeftBorder} />
+                    <Image src={topRightBorder} alt="" className={styles.topRightBorder} />
+                </div>
+                <Image src={bottomBorder} alt="" className={styles.bottomBorder} />
+            </div>
+            <div className={styles.sponsorParent}>
+                <div className={styles.title}>
+                    <div className={styles.titleBorder}>{name}</div>
+                </div>
+                <div className={styles.body}>
+                    {logo != '' && <Image src={logo} alt={name} width={400} height={400} />}
+                </div>
+            </div>
+        </div>
+    );
+};
+export default SponsorCard;
