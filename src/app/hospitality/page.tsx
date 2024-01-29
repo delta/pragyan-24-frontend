2c26010abda624a3703f48e7c9b2a464ae3b91e7
Ayush Chadha <elegantShock3307@gmail.com>
2024-01-29 13:31:39 +0530

    fix: padding and menubar

--- src/app/hospitality/page.tsx
+++ src/app/hospitality/page.tsx
@@ -25,85 +25,84 @@ export default function Hospitality() {
     return (
         <>
             {content.length > 0 && (
-                <div className={styles.parent}>
+                <div className={`min-h-screen w-full text-center max-sm:p-3 p-5 ${styles.parent}`}>
                     <NavBar />
-                    <div className={styles.content}>
-                        <div className={styles.title}>Hospitality </div>
-                        <div className={styles.tabContent}>
-                            {!mobile ? (
-                                <div className={styles.tabPaneWrapper}>
-                                    <div className={styles.tabPane}>
-                                        <div className="flex justify-around">
-                                            {content.map(({ name: tabName }, index) => {
-                                                return (
-                                                    <div
-                                                        className={styles.tab}
-                                                        key={index}
-                                                        id={`tab${index}`}
-                                                        onClick={() => {
-                                                            setTabSelected(index);
-                                                        }}
-                                                    >
-                                                        {tabName}
-                                                    </div>
-                                                );
-                                            })}
-                                        </div>
-                                        {document != undefined && (
-                                            <motion.div
-                                                initial={{
-                                                    x: document.getElementById(`tab${tabSelected}`)
-                                                        ?.offsetLeft,
-                                                    width: document
-                                                        .getElementById(`tab${tabSelected}`)
-                                                        ?.getBoundingClientRect().width,
-                                                }}
-                                                animate={{
-                                                    x: document.getElementById(`tab${tabSelected}`)
-                                                        ?.offsetLeft,
-                                                    width: document
-                                                        .getElementById(`tab${tabSelected}`)
-                                                        ?.getBoundingClientRect().width,
-                                                }}
-                                                transition={{
-                                                    type: 'spring',
-                                                    stiffness: 100,
-                                                }}
-                                                className={styles.underline}
-                                            />
-                                        )}
+                    <div className="font-ROG 2xl:text-6xl xl:text-5xl lg:text-4xl sm:text-3xl text-2xl mt-12 flex justify-center align-middle">
+                        HOSPITALITY
+                    </div>
+                    <div className={styles.tabContent}>
+                        {!mobile ? (
+                            <div className={styles.tabPaneWrapper}>
+                                <div className={styles.tabPane}>
+                                    <div className="flex justify-around">
+                                        {content.map(({ name: tabName }, index) => {
+                                            return (
+                                                <div
+                                                    className={styles.tab}
+                                                    key={index}
+                                                    id={`tab${index}`}
+                                                    onClick={() => {
+                                                        setTabSelected(index);
+                                                    }}
+                                                >
+                                                    {tabName}
+                                                </div>
+                                            );
+                                        })}
                                     </div>
-                                </div>
-                            ) : (
-                                <div className={styles.tabPaneWrapper}>
-                                    <div className={styles.tabTitle}>
-                                        <Image
-                                            src={leftArrow}
-                                            alt="lArrow"
-                                            className="self-start"
-                                            width={40}
-                                            onClick={() => {
-                                                setTabSelected(
-                                                    (content.length + tabSelected - 1) %
-                                                        content.length,
-                                                );
+                                    {document != undefined && (
+                                        <motion.div
+                                            initial={{
+                                                x: document.getElementById(`tab${tabSelected}`)
+                                                    ?.offsetLeft,
+                                                width: document
+                                                    .getElementById(`tab${tabSelected}`)
+                                                    ?.getBoundingClientRect().width,
                                             }}
-                                        />
-                                        <div> {content[tabSelected].name} </div>
-                                        <Image
-                                            src={rightArrow}
-                                            alt="rArrow"
-                                            width={40}
-                                            className="self-end"
-                                            onClick={() => {
-                                                setTabSelected((tabSelected + 1) % content.length);
+                                            animate={{
+                                                x: document.getElementById(`tab${tabSelected}`)
+                                                    ?.offsetLeft,
+                                                width: document
+                                                    .getElementById(`tab${tabSelected}`)
+                                                    ?.getBoundingClientRect().width,
                                             }}
+                                            transition={{
+                                                type: 'spring',
+                                                stiffness: 90,
+                                            }}
+                                            className={styles.underline}
                                         />
-                                    </div>
+                                    )}
                                 </div>
-                            )}
-                            <div className={styles.desc}>{content[tabSelected].desc}</div>
-                        </div>
+                            </div>
+                        ) : (
+                            <div className={styles.tabPaneWrapper}>
+                                <div className={styles.tabTitle}>
+                                    <Image
+                                        src={leftArrow}
+                                        alt="lArrow"
+                                        className="self-start"
+                                        width={40}
+                                        onClick={() => {
+                                            setTabSelected(
+                                                (content.length + tabSelected - 1) % content.length,
+                                            );
+                                        }}
+                                    />
+                                    <div> {content[tabSelected].name} </div>
+                                    <Image
+                                        src={rightArrow}
+                                        alt="rArrow"
+                                        width={40}
+                                        className="self-end"
+                                        onClick={() => {
+                                            setTabSelected((tabSelected + 1) % content.length);
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        )}
+                        <div className={styles.desc}>{content[tabSelected].desc}</div>
                     </div>
                 </div>
             )}
